9255f2e9e03795c171483e0500fc835c81c12b99
Naomi TODORI <naomi.todori@atmark-techno.com>
2020-08-04 14:47:57 +0900

    HLApp/RTApp/InitFW : Change to support Azure Sphere SDK 20.07

--- Firmware/HLApp/Cactusphere_100/main.c
+++ Firmware/HLApp/Cactusphere_100/main.c
@@ -786,6 +786,9 @@ static const char *GetReasonString(IOTHUB_CLIENT_CONNECTION_STATUS_REASON reason
     case IOTHUB_CLIENT_CONNECTION_OK:
         reasonString = "IOTHUB_CLIENT_CONNECTION_OK";
         break;
+    case IOTHUB_CLIENT_CONNECTION_NO_PING_RESPONSE:
+        reasonString = "IOTHUB_CLIENT_CONNECTION_NO_PING_RESPONSE";
+        break;
     }
     return reasonString;
 }
